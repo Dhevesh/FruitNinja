c9beeb37096db526ccc2b1f1b0249d40e180a013
IMS\Dheveshp <dheveshp@gmail.com>
2019-05-21 15:41:57 +0100

    Main functionality added. fruits spawn. fruits can be cut with press of a button. cloned objects are destroyed.

---
+++ FruitNinja/Assets/Scripts/FruitController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitController : MonoBehaviour
+{
+    public GameObject slicedFruitPrefab;
+
+    public void CreateSlicedFruit()
+    {
+        GameObject slicedFruitInstance = Instantiate(slicedFruitPrefab, transform.position, transform.rotation) as GameObject;
+        Rigidbody[] rbs = slicedFruitInstance.transform.GetComponentsInChildren<Rigidbody>();
+        foreach (Rigidbody rb in rbs)
+        {
+            rb.transform.rotation = Random.rotation;
+            rb.AddExplosionForce(Random.Range(500, 1000), transform.position, 5f);
+        }
+        Destroy(gameObject);
+        Destroy(slicedFruitInstance, 5f);
+    }
+
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            CreateSlicedFruit();
+        }
+    }
+}
---
+++ FruitNinja/Assets/Scripts/FruitSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitSpawner : MonoBehaviour
+{
+    public GameObject[] fruitToSpawn;
+    public Transform[] spawnPlaces;
+    public float minWaitTime = 0.3f;
+    public float maxWaitTime = 1f;
+    public float minForce = 10f;
+    public float maxForce = 20f;
+    void Start()
+    {
+        StartCoroutine("SpawnedFruits");
+    }
+
+    private IEnumerator SpawnedFruits()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minWaitTime, maxWaitTime));
+            Transform t = spawnPlaces[Random.Range(0, spawnPlaces.Length)];
+            int objectIndex = Random.Range(0, fruitToSpawn.Length);
+            GameObject fruits = Instantiate(fruitToSpawn[objectIndex], t.transform.position, t.transform.rotation) as GameObject; // to add to a list to delete
+            fruits.GetComponent<Rigidbody2D>().AddForce(t.transform.up * Random.Range(minForce,maxForce), ForceMode2D.Impulse);
+            Destroy(fruits, 5f);
+        }
+    }
+}
