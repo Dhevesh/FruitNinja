1acdeef9f500665f5ebd87ec161f9d7914c0fce9
IMS\Dheveshp <dheveshp@gmail.com>
2019-05-22 13:59:07 +0100

    Implented timer in game to end the game.

--- FruitNinja/Assets/Scripts/BladeController.cs
+++ FruitNinja/Assets/Scripts/BladeController.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class BladeController : MonoBehaviour
 {
+    public float bladeVelocity = 0.1f;
 
     private Rigidbody2D rb;
+    private Collider2D bladeCollider;
+    private Vector3 lastBladePos;
+
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        bladeCollider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         SetBladeToMouse();
+        bladeCollider.enabled = isBladeMoving();
     }
 
     private void SetBladeToMouse()
@@ -23,5 +29,16 @@ public class BladeController : MonoBehaviour
         var mousePos = Input.mousePosition;
         mousePos.z = 10;
         rb.position = Camera.main.ScreenToWorldPoint(mousePos);
+    }
+
+    private bool isBladeMoving()
+    {
+        Vector3 currentBladePos = transform.position;
+        float bladeTravelled = (lastBladePos - currentBladePos).magnitude;
+        lastBladePos = currentBladePos;
+        if (bladeTravelled > bladeVelocity)
+            return true;
+        else
+            return false;
     }
 }
--- FruitNinja/Assets/Scripts/GameManager.cs
+++ FruitNinja/Assets/Scripts/GameManager.cs
@@ -8,29 +8,54 @@ public class GameManager : MonoBehaviour
     [Header ("Game")]
     public Text scoreText;
     public Text highScoreText;
-    private int score;
+    public Text timeText;
+    public int score;
+    public int highScore;
+    public float time = 3000f;
 
     [Header("GameOverPanel")]
-    public Text gameOverScoreText;
     public GameObject gameOverPanel;
+    public Text gameOverScoreText;
+    public Text gameOverHighScoreText;
+    
 
+    void Update()
+    {
+        if (Time.timeScale != 0)
+        {
+            time--;
+            if (time == 0)
+            {
+                OnBombHit();
+                time = 3000f;
+            }
+            timeText.text = $"Time Remaining: {Mathf.RoundToInt((time / 100)).ToString()}s";
+        }
+        
+    }
     void Awake()
     {
         gameOverPanel.SetActive(false);
+        highScore = PlayerPrefs.GetInt("HighScore");
+        highScoreText.text = $"Best: {highScore}";
     }
     public void IncreaseScore()
     {
         score++;
         scoreText.text = $"Current Score: {score.ToString()}";
+        if (score > highScore)
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+            highScoreText.text = $"Best: {score.ToString()}";
+        }
     }
 
     public void OnBombHit()
     {
-
         Time.timeScale = 0;
         gameOverPanel.SetActive(true);
         gameOverScoreText.text = $"Score: {score}";
-
+        gameOverHighScoreText.text = $"Best: {PlayerPrefs.GetInt("HighScore").ToString()}";
     }
 
     public void OnRestartClick()
@@ -43,6 +68,8 @@ public class GameManager : MonoBehaviour
         {
             Destroy(go);
         }
+        time = 3000f;
         Time.timeScale = 1;
+
     }
 }
